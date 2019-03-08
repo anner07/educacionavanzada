3c06f71de5bfd385a3bd2c5b4e2223cf59f9eb1e
anner <torresmorel7@gmail.com>
2019-03-07 20:08:18 -0600

    validaciones y agregando imagenes

---
+++ educacionavanzada-master/EducacionAvanzada/EducacionAvanzada.BL/Contexto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace EducacionAvanzada.BL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto() : base(@"Data Source=(LocalDb)\MSSQLLocalDB;AttachDBFilename=" +
+            Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\EducacionAvanzadaDB.mdf")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+        public DbSet<Alumno> Alumnos { get; set; }
+        public DbSet<Grado> Grados { get; set; }
+        public DbSet<Jornada> Jornadas { get; set; }
+    }
+
+}
